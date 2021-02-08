a3fd843a30f60e7e3b6271cef2b07d9f78129ae5
I538957 <Yannisrtmr@gmail.com>
2021-02-08 20:01:11 +0100

    First test Commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-from kivy.app import App
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+
+
+class MainApp(MDApp):
+    def build(self):
+        return MDLabel(text="Hello World", halign="center")
+
+
+MainApp().run()
